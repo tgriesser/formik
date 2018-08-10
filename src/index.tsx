d6225895fddbd854b1b78faebcbe5a7a8489fa81
Tim Griesser <tgriesser10@gmail.com>
2018-08-10 10:14:03 -0400

    Remove withFormik

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 export * from './Formik';
 export * from './Field';
 export * from './Form';
-export * from './withFormik';
 export * from './FieldArray';
 export * from './utils';
 export * from './FastField';
